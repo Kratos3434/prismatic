964cf4ff4d591154b0a4fcb56b5fcf0910c69f82
keith carlos <keithcarlos34@gmail.com>
2024-01-06 03:19:18 -0500

    Likes v2

--- routes/public/user.js
+++ routes/public/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const user = require('../../controller/user');
 const otp = require('../../controller/otp');
+const post = require('../../controller/post');
 const auth = require('../../auth');
 
 router.post("/signup", user.signup);
@@ -12,5 +13,5 @@ router.get("/token/verify/:token", user.verifyResetToken);
 router.post("/forgot/password", user.forgotPassword);
 router.post("/resend/otp/:email", auth.authOtp, otp.resend);
 router.get("/user/:name", user.getByName);
-
+router.get("/post/:id", post.getById);
 module.exports = router;
